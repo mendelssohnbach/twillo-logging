77722757aceaa013f42c9d8bfe2be7f4a7e3cd05
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-29 17:56:11 +0900

    doc: 名前空間の利用例

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const pino = require('pino');
 const expressPino = require('express-pino-logger');
 
+const randomId = require('./random-id');
+
 // `logger`インスタンスを作成
 const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
 // `express-pino-logger`に割当
@@ -16,7 +18,8 @@ app.use(expressLogger);
 app.get('/', (req, res) => {
   // `logger.debug`をルートパスに追加
   logger.debug('Calling res.send');
-  res.send('Hello World');
+  const id = randomId.getRandomId();
+  res.send(`Hello World [${id}]`);
 });
 
 app.listen(PORT, () => {
