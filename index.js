a622f2ea513850936b4ad49f4cea0a0b9cfb8b59
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-29 16:32:27 +0900

    fix pinoによるロギング

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
+
+// `logger`インスタンスを作成
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+// `express-pino-logger`に割当
+const expressLogger = expressPino({ logger });
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('%s', req); // %o:オブジェクト全体
-  next();
-});
+// 新しいロガーミドルウェアを作成
+app.use(expressLogger);
 
 app.get('/', (req, res) => {
+  // `logger.debug`をルートパスに追加
+  logger.debug('Calling res.send');
   res.send('Hello World');
 });
 
 app.listen(PORT, () => {
-  console.log('Server running on port %d', PORT);
+  logger.info('Server running on port %d', PORT);
 });
